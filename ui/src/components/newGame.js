793a5ff84e427d9af432a036b8b5ff3bf7e2dc52
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-04-08 22:59:15 -0700

    Added newGame.css

--- ui/src/components/newGame.js
+++ ui/src/components/newGame.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import './newGame.css'
 
 class NewGame extends Component {
   render() {
     return(
       <div>
-        <div>
+        <div className='GameHeader'>
           <h2> Start New Game </h2>
         </div>
+        <div className="GameHeader">
           <h2> Join New Game </h2>
+        </div>
       </div>
     )
   }
