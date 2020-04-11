24ea9034eaf3f7aa9e913a0ec34d2214732655c4
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-04-10 20:40:02 -0700

    Form now updates state when number of opponents is changed.

--- ui/src/components/newGameForm/newGameForm.js
+++ ui/src/components/newGameForm/newGameForm.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 
 class NewGameForm extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {numberOfOpponents: "one"};
+
+    this.changeNumberOpponents = this.changeNumberOpponents.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
   handleSubmit(event) {
     event.preventDefault();
+  }
+
+  changeNumberOpponents(event) {
+    this.setState({ numberOfOpponents: event.target.value })
   }
 
   render() {
@@ -11,10 +23,10 @@ class NewGameForm extends Component {
       <div className="NewGame">
         <form onSubmit={this.handleSubmit}>
           <label for="number of players">Number of Opponents</label>
-          <select id="numberOfPlayers" name="numberOfPlayers">
-            <option value="one">two</option>
-            <option value="two">three</option>
-            <option value="three">four</option>
+          <select id="numberOfPlayers" name="numberOfPlayers" onChange={this.changeNumberOpponents}>
+            <option value="one">one</option>
+            <option value="two">two</option>
+            <option value="three">three</option>
           </select>
           <input type="submit" value="Submit"/>
         </form>
