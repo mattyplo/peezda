6af6b3072ed19b1c975fe24e22b61f10331b3e40
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-04-14 20:55:25 -0700

    Added Router and Switches to app.js.

--- ui/src/App.js
+++ ui/src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './components/home.js';
+import Game from './components/Game/Game.js';
 import Header from './components/header.js';
-import Footer from './components/footer.js'
+import Footer from './components/footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/match" component={Game} />
+        </Switch>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
