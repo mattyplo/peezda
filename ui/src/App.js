9f8a72ab2bac243b021ec07da6382a37b4a7f1a0
mattyplo <matthew.ploszaj@seattlecentral.edu>
2020-04-14 20:43:21 -0700

    Added react-router-dom to project.  Created the PlayerCard component.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from './components/home.js';
 import Header from './components/header.js';
---
+++ ui/src/components/PlayerCard/PlayerCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class PlayerCard extends Component {
+
+  render() {
+    return (
+      <div>
+        <h3>Player One</h3>
+        <h3>Score: 0</h3>
+      </div>
+    )
+  }
+}
+
+export default PlayerCard;
